c3ccd58b7d795d6ec42bba5d4008ea232f024ef0
Максим Стрельников <maksim_stv@mail.ru>
2021-11-01 21:42:47 +0300

    Modified variant 1.2

--- 3_sem/AisD/lab_2/Second_step_of_evolution/Second_step_of_evolution.cpp
+++ 3_sem/AisD/lab_2/Second_step_of_evolution/Second_step_of_evolution.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <time.h>
 #include <stdlib.h> 
+#include <chrono>
 
 int BinarySearch(int* Array, int Size_array, int key) {
     char flag = 'N';
@@ -50,34 +51,159 @@ void QuickSort(int* array, int low, int high)
         QuickSort(array, left, high);
 }
 
+void BubbleSort(int* Array, int Size_array) {
+    int tmp;
+    /*for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 9; j++) {
+            if (Array[j] > Array[j + 1]) {
+                tmp = Array[j]; // создали дополнительную переменную
+                Array[j] = Array[j + 1]; // меняем местами
+                Array[j + 1] = tmp; // значения элементов
+            }
+        }
+    }*/
+    for (int i = 1; i < Size_array; i++) {
+        for (int j = Size_array - 1; j >= i; j--) {
+            if (Array[j - 1] > Array[j]) {
+                tmp = Array[j - 1];
+                Array[j - 1] = Array[j];
+                Array[j] = tmp;
+            }
+        }
+    }
+}
+
+
+
+
+void InsertionSort(int* array, int arraySize)
+{
+    int temp, i, j;
+    for (i = 1; i < arraySize; i++)
+    {
+        temp = array[i];
+        j = i - 1;
+
+        while (j >= 0 && array[j] > temp)
+        {
+            array[j + 1] = array[j];
+            j = j - 1;
+        }
+        array[j + 1] = temp;
+    }
+}
 
 int main() {
-    srand(time(NULL));
-    int LuckyPanch = 0;
-    for (int i = 0; i < 10000; i++) {
-        int* array = new int[10];
-        for (int index = 0; index < 10; index++) {
-            array[index] = rand() % 1000;
-        }
-        for (int index = 0; index < 10; index++) {
-            std::cout << array[index] << " ";
-        }
-        std::cout << std::endl;
-        QuickSort(array, 0, 9);
-        for (int index = 0; index < 10; index++) {
-            std::cout << array[index] << " ";
-        }
-        std::cout << std::endl;
-        // андрей уже отсортировал, приступаем к двоичной сортировке
-        if (BinarySearch(array, 10, 15) == -1) {
-            std::cout << "Ti popusk!!!\n";
-        }
-        else { std::cout << "Good job!!!\n";  LuckyPanch++; }
-        delete[] array;
-        std::cout << std::endl;
-    }
 
-    std::cout << LuckyPanch;
+    //srand(time(NULL));
+
+    for (int i = 0; i < 10; i++) {
+        srand(time(NULL));
+        double Average_1, Average_2, Average_3;
+        Average_1 = Average_2 = Average_3 = 0;
+
+        /*for (int i = 0; i < 10000; i++) {
+            int* array = new int[10];
+            for (int index = 0; index < 10; index++) {
+                array[index] = rand() % 1000;
+            }
+            for (int index = 0; index < 10; index++) {
+                std::cout << array[index] << " ";
+            }
+            std::cout << std::endl;
+            QuickSort(array, 0, 9);
+            for (int index = 0; index < 10; index++) {
+                std::cout << array[index] << " ";
+            }
+            std::cout << std::endl;
+            // андрей уже отсортировал, приступаем к двоичной сортировке
+            if (BinarySearch(array, 10, 15) == -1) {
+                std::cout << "Ti popusk!!!\n";
+            }
+            else { std::cout << "Good job!!!\n";  LuckyPanch++; }
+            delete[] array;
+            std::cout << std::endl;
+        }*/
+
+        for (int j = 0; j < 10; j++) {
+
+            int* array_1 = new int[1000];
+            int* array_2 = new int[1000];
+            int* array_3 = new int[1000];
+            for (int index = 0; index < 1000; index++) {
+                array_1[index] = rand() % 100;
+                array_2[index] = array_1[index];
+                array_3[index] = array_1[index];
+            }
+
+
+            for (int index = 0; index < 10; index++) {
+                std::cout << array_1[index] << " ";
+            }
+            std::cout << std::endl;
+            auto  now = std::chrono::high_resolution_clock::now();
+            QuickSort(array_1, 0, 9);
+            for (int index = 0; index < 10; index++) {
+                std::cout << array_1[index] << " ";
+            }
+            std::cout << std::endl;
+            auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - now);
+            Average_1 += elapsed.count();
+            std::cout << "QuickSort: " << elapsed.count() << "ns.\n";
+            std::cout << std::endl; std::cout << std::endl;
+
+
+
+
+
+
+
+            for (int index = 0; index < 10; index++) {
+                std::cout << array_2[index] << " ";
+            }
+            std::cout << std::endl;
+            now = std::chrono::high_resolution_clock::now();
+            BubbleSort(array_2, 10);
+            for (int index = 0; index < 10; index++) {
+                std::cout << array_2[index] << " ";
+            }
+            std::cout << std::endl;
+            elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - now);
+            Average_2 += elapsed.count();
+            std::cout << "BubbleSort: " << elapsed.count() << "ns.\n";
+            std::cout << std::endl;
+            std::cout << std::endl;
+
+
+
+
+
+
+            for (int index = 0; index < 10; index++) {
+                std::cout << array_3[index] << " ";
+            }
+            std::cout << std::endl;
+            now = std::chrono::high_resolution_clock::now();
+            InsertionSort(array_3, 10);
+            for (int index = 0; index < 10; index++) {
+                std::cout << array_3[index] << " ";
+            }
+            std::cout << std::endl;
+            elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - now);
+            Average_3 += elapsed.count();
+            std::cout << "InsertionSort: " << elapsed.count() << "ns.\n";
+            std::cout << std::endl;
+            std::cout << std::endl;
+
+
+            delete[] array_1;
+            delete[] array_2;
+            delete[] array_3;
+
+        }
+
+        std::cout << Average_1 / 100 << "\t" << Average_2 / 100 << "\t" << Average_3 / 100 << "\n";
+    }
     system("pause");
     return 0;
 }

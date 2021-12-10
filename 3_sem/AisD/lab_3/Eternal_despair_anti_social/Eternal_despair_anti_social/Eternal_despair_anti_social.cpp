a6ec59a00357114ed47439e8bc0581b2613f22c4
Максим Стрельников <maksim_stv@mail.ru>
2021-12-11 00:27:43 +0300

    coursovik

---
+++ 3_sem/AisD/lab_3/Eternal_despair_anti_social/Eternal_despair_anti_social/Eternal_despair_anti_social.cpp
@@ -0,0 +1,19 @@
+﻿// Eternal_despair_anti_social.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string str1 = "123.0";
+    std::string str2 = "0.123";
+
+    auto m1 = std::stod(str1);
+    auto m2 = std::stod(str2);
+
+    std::cout << m1 + m2;
+
+    system("pause");
+    return 0;
+}

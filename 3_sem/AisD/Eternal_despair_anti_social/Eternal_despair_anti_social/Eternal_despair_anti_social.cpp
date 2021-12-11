5c9d4ac949139df7b1b081724d810cf10469ab89
Максим Стрельников <maksim_stv@mail.ru>
2021-12-12 02:11:11 +0300

    993-7

--- 3_sem/AisD/Eternal_despair_anti_social/Eternal_despair_anti_social/Eternal_despair_anti_social.cpp
+++ 3_sem/AisD/Eternal_despair_anti_social/Eternal_despair_anti_social/Eternal_despair_anti_social.cpp
@@ -3,16 +3,92 @@
 
 #include <iostream>
 #include <string>
+#include "Stack.h"
 
 int main()
 {
-    std::string str1 = "123.0";
+    Stack<std::string> stack;
+
+    std::string str1 = "123 + (12 - 13.5) / (15 - 12)";
     std::string str2 = "0.123";
 
-    auto m1 = std::stod(str1);
-    auto m2 = std::stod(str2);
+    
 
-    std::cout << m1 + m2;
+    int left, right;
+    left = 0; right = 0;
+    for (int i = 0; i < str1.length(); i++) { // собственно проверка на адекватность пользователя
+        if (str1[i] == '(') {
+            left++;
+        }
+        if (str1[i] == ')') {
+            right++;
+        }
+    }
+    if (left != right) {
+        throw std::runtime_error("the number on the right-parenthesis does not match the number on the lesf-parenthesis\n");
+    }
+    
+    // пiшла вода в хату
+    /*Создать пустой стек для хранения операторов
+        Просматриваем выражение справа налево
+        - если токен является правой скобкой, поместить его в стэк
+        - если токен – операнд, добавить его в выходной список
+        - если токен является левой скобкой, извлечь из стэка все символы до
+        появления правой скобки и добавить их в выходной поток
+        - если токен является оператором, удалить все операторы из стэка (пока не
+        увидим оператор с меньшим приоритетом) и поместить их в выходной
+        поток, если и только если приоритет оператора, который находится
+        наверху стэка, больше или равен приоритета просматриваемого
+        оператора. Затем поместить просматриваемый оператор в стэк. Иначе
+        поместить просматриваемый оператор в стэк сразу
+        - если после просмотра всех элементов остались операторы в стэке, то
+        вывести их в выходной список
+    */
+
+    std::string temp;
+    std::string temp_parenthesis;
+    /*Stack<std::string> stroka;
+    std::string temp;
+    for (int i = str1.length() - 1; i >= 0; i--) {
+        if (str1[i] == ')') {
+            temp = str1[i];
+            stroka.push(temp);
+        }
+        if()
+    }*/
+
+
+
+    // ты в муте чел....
+    for (int i = 0; i < str1.length(); i++) {
+        if ((str1[i] != ' ')&&(str1[i] != ')')&&(str1[i] != '(')) {
+            temp += str1[i];
+        }
+        if (str1[i] == ')') {
+            stack.push(temp);
+            temp = "\0";
+            temp_parenthesis = '(';
+            stack.push(temp_parenthesis);
+        }
+
+        if  (str1[i] == '(') {
+            temp_parenthesis = ')';
+            stack.push(temp_parenthesis);
+        }
+        
+        if (str1[i] == ' ') {
+            stack.push(temp);
+            temp = "\0";
+        }
+    }
+    stack.push(temp); // 1000-7
+
+
+    while (!stack.isEmpty()) {
+        std::cout<<stack.peek();
+        stack.pop();
+    }
+
 
     system("pause");
     return 0;
---
+++ 3_sem/AisD/Eternal_despair_anti_social/Eternal_despair_anti_social/Stack.h
@@ -0,0 +1,104 @@
+#ifndef STACK_H
+#pragma once
+
+#include<iostream>
+
+template <class T>
+class Stack
+{
+private:
+
+	class Elem_stack
+	{
+	private:
+		T data;
+		Elem_stack* next;
+
+	public:
+
+		Elem_stack(T data, Elem_stack* next = NULL) : data(data), next(next) {}
+		~Elem_stack() = default;
+
+		void setNext(Elem_stack* newnext) {
+			this->next = newnext;
+		}
+
+		Elem_stack* getNext() {
+			return this->next;
+		}
+
+		T getElemdata() {
+			return this->data;
+		}
+	};
+
+	Elem_stack* head;
+public:
+	Stack();
+
+	~Stack();
+
+	void pop();
+
+	void push(T data);
+
+	bool isEmpty();
+
+	T peek();
+};
+
+template <class T> Stack<T>::Stack()
+{
+	head = NULL;
+}
+
+template <class T>
+Stack<T>::~Stack()
+{
+	while (!isEmpty()) {
+		pop();
+	}
+}
+
+template <class T> void Stack<T>::push(T data)
+{
+	Elem_stack* temp = new Elem_stack(data);
+	Elem_stack* cur = head; // 1
+	this->head = temp; // 2
+	this->head->setNext(cur); // 3
+
+}
+
+template <class T> void Stack<T>::pop()
+{
+	if (isEmpty()) {
+		throw std::out_of_range("No more elements");
+	}
+	else {
+		Elem_stack* temp = head;
+		head = head->getNext();
+		delete temp;
+	}
+}
+
+template <class T> bool Stack<T>::isEmpty()
+{
+	if (head == NULL) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+template <class T> T Stack<T>::peek()
+{
+	if (isEmpty()) {
+		throw std::out_of_range("Empty");
+	}
+	else {
+		return head->getElemdata();
+	}
+
+}
+#endif // !STACK_H

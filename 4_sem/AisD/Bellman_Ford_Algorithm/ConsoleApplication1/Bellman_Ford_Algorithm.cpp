5e9b9379abaadee0415c76baba62b470e2d33ff9
Максим Стрельников <maksim_stv@mail.ru>
2022-04-22 00:17:16 +0300

    secret file

---
+++ 4_sem/AisD/Bellman_Ford_Algorithm/ConsoleApplication1/Bellman_Ford_Algorithm.cpp
@@ -0,0 +1,82 @@
+﻿// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <fstream>
+#include "LinkedList.h"
+
+struct Edge {
+    std::string from;
+    std::string to;
+    float cost = 0;
+};
+
+void parsePriceList(List<Edge>* adjacencyMatrix, List<std::string>* vertices) {
+    std::fstream priceList;
+
+    priceList.open("PriceList.txt", std::ios_base::in);
+    if (priceList.bad()) {
+        std::cout << "Error when opening a file for input!\n";
+    }
+    else {
+        std::string line;
+        std::string subline;
+        std::string::size_type posit = 0;
+        while (std::getline(priceList, line))
+        {   
+            Edge edge_1;
+            Edge edge_2;
+            
+            subline = line;
+            posit = line.find(';');
+            subline.resize(posit);
+            vertices->push_back(subline);
+            edge_1.from = subline;
+            edge_2.to = subline;
+            line.erase(0, posit+1);
+
+            subline = line;
+            posit = line.find(';');
+            subline.resize(posit);
+            vertices->push_back(subline);
+            edge_1.to = subline;
+            edge_2.from = subline;
+            line.erase(0, posit + 1);
+                
+            subline = line;
+            posit = line.find(';');
+            subline.resize(posit);
+            if (subline != "N/A") {
+                edge_1.cost = std::stof(subline);
+                adjacencyMatrix->push_front(edge_1);
+            }
+                
+            line.erase(0, posit + 1);
+            if (line != "N/A") {
+                edge_2.cost = std::stof(line);
+                adjacencyMatrix->push_front(edge_2);
+            }
+        }
+    }
+    priceList.close();
+}
+
+
+
+int main()
+{
+    List<Edge> adjacencyMatrix; // get_size = edges count
+    List<std::string> vertices; // get_size = vertices count
+
+    
+
+    parsePriceList(&adjacencyMatrix, &vertices);
+
+    std::cout << adjacencyMatrix.get_size() << std::endl;
+    std::cout << vertices.get_size() << std::endl;
+    
+    system("pause");
+    return 0;
+}
---
+++ 4_sem/AisD/Bellman_Ford_Algorithm/ConsoleApplication1/LinkedList.h
@@ -0,0 +1,69 @@
+#pragma once
+#ifndef LINKEDLIST_H
+#pragma once
+
+#include <iostream>
+#include "LinkedListElem.h"
+
+template<class T_link>
+class List {
+private:
+	Elem_list<T_link>* head, * last, * cur;
+
+public:
+	List() { // create blank list
+		this->head = NULL;
+		this->last = this->head;
+		this->cur = this->head;
+	}
+
+	List(T_link data) { // create list with one element
+		Elem_list<T_link>* elem = new Elem_list(data);
+		this->head = elem;
+		this->last = elem;
+		this->cur = elem;
+	}
+
+	void push_front(T_link item);
+
+	void push_back(T_link item);
+
+	void pop_front();
+
+	void pop_back();
+
+	void insert(T_link item, size_t index);
+
+	int at(size_t index);
+
+	void remove(size_t index);
+
+	size_t get_size();
+
+	void clear();
+
+	void set(size_t index, T_link new_data);
+
+	bool isEmpty();
+
+	friend std::ostream& operator<< (std::ostream& out, const List& printList);
+
+	void reverse();
+
+	~List() = default;
+
+	/*~List() {
+		Elem_list<T_link>* elem = head;
+		Elem_list<T_link>* tmp;
+		while (elem != NULL) {
+			tmp = elem;
+			elem = elem->getNext();
+			delete tmp;
+		}
+	}*/
+
+};
+
+#include "LinkedList.cpp"
+
+#endif // !LINKEDLIST_H

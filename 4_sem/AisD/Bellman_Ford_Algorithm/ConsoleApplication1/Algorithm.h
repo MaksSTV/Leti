1208a7a26b3539f5b7c7227b8d00cb5c1675e111
Максим Стрельников <maksim_stv@mail.ru>
2022-04-29 15:53:46 +0300

    fix_lab3

--- 4_sem/AisD/Bellman_Ford_Algorithm/ConsoleApplication1/Algorithm.h
+++ 4_sem/AisD/Bellman_Ford_Algorithm/ConsoleApplication1/Algorithm.h
@@ -5,14 +5,14 @@ int countUniqueVertex(List<std::string>* vertices) {
 
     if (!vertices->isEmpty()) {
 
-        size_t n = vertices->get_size();
+        size_t countNotUniqueVertex = vertices->get_size();
 
-        for (int i = 0; i < n - 1; i++)
-            for (int j = i + 1; j < n; j++)
+        for (int i = 0; i < countNotUniqueVertex - 1; i++)
+            for (int j = i + 1; j < countNotUniqueVertex; j++)
                 if (vertices->at(i) == vertices->at(j))
                 {
                     vertices->remove(j);
-                    n--;
+                    countNotUniqueVertex--;
                     j--;
                 }
 
@@ -25,11 +25,11 @@ int countUniqueVertex(List<std::string>* vertices) {
 
 void setUniqueNumberTown(List<Edge>* edge, List<std::string>* vertices) {
 
-    size_t n = edge->get_size();
-    size_t m = vertices->get_size();
+    size_t uniqueNumberTown = edge->get_size();
+    size_t countUniqueVertex = vertices->get_size();
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+    for (int i = 0; i < uniqueNumberTown; i++) {
+        for (int j = 0; j < countUniqueVertex; j++) {
             if (edge->at(i).from == vertices->at(j)) {
                 edge->at(i).numberFromTown = j;
             }
@@ -42,43 +42,44 @@ void setUniqueNumberTown(List<Edge>* edge, List<std::string>* vertices) {
 
 int algorithm(List<Edge>* edge, List<std::string>* vertices, std::string from, std::string to) {
 
-    size_t n = countUniqueVertex(vertices);
-    size_t m = edge->get_size();
+    size_t countVertex = countUniqueVertex(vertices);
+    size_t countEdge = edge->get_size();
 
     setUniqueNumberTown(edge, vertices);
 
-    int* d = new int[n];
+    int* matrixCost = new int[countVertex];
     int conditionalInfinity = INT_MAX - 10000;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < countVertex; i++) {
         if (vertices->at(i) == from) {
-            d[i] = 0;
+            matrixCost[i] = 0;
         }
         else {
-            d[i] = conditionalInfinity;
+            matrixCost[i] = conditionalInfinity;
         }
     }
 
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < m; j++) {
-            if (d[edge->at(j).numberFromTown] + edge->at(j).cost < d[edge->at(j).numberToTown]) {
-                d[edge->at(j).numberToTown] = d[edge->at(j).numberFromTown] + edge->at(j).cost;
+    for (int i = 0; i < countVertex - 1; i++) {
+        for (int j = 0; j < countEdge; j++) {
+            if (matrixCost[edge->at(j).numberFromTown] + edge->at(j).cost < matrixCost[edge->at(j).numberToTown]) {
+                matrixCost[edge->at(j).numberToTown] = matrixCost[edge->at(j).numberFromTown] + edge->at(j).cost;
             }
         }
     }
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < countVertex; i++) {
         if (from == to) {
             std::cout << std::endl << from << "->" << to << "=" << "Not path" << std::endl;
             return conditionalInfinity;
         }
         else {
             if (vertices->at(i) == to) {
-                std::cout << std::endl << from << "->" << to << "=" << d[i] << std::endl;
-                return d[i];
+                std::cout << std::endl << from << "->" << to << "=" << matrixCost[i] << std::endl;
+                return matrixCost[i];
             }
         }
     }
+    delete[] matrixCost;
 }
 
 
